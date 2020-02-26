c7e8e955afd4e1e50aacbe79c6d13793714f6f2c
i.lukin <i.lukin@rj-games.com>
2020-02-26 20:02:25 +0300

    add arguments to main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-
+    if (argc < 2)
+    {
+        std::cerr << "program expect bulk size argument!" << std::endl;
+        return EXIT_FAILURE;
+    }
 }
